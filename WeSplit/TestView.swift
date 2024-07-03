0a02d2b79a9148432e2436bd8c2a271e6b228477
QinYYY <qinyang_c@outlook.com>
2024-07-03 16:01:45 +0930

    1

---
+++ WeSplit/TestView.swift
@@ -0,0 +1,54 @@
+//
+//  TestView.swift
+//  WeSplit
+//
+//  Created by QinY on 3/7/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    let array = ["one","two","three"]
+    @State private var number = "one"
+    @State private var tapCount = 0
+    @State private var name = ""
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Text("this is a form")
+                Section{
+                    Text("this is a section")
+                }
+                Section{
+                    Text("this is a section")
+                    Text("this is a section")
+                }
+                Section{
+                    Button("tapCount\(tapCount)") {
+                        self.tapCount += 1
+                    }
+                }
+                Section{
+                    TextField("enter your name", text: $name)
+                    Text("your name is \(name)")
+                }
+                Section{
+                    Picker("选择你的号码",selection: $number){
+                       
+                        ForEach(array,id:\.self){
+                            Text("\($0)")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.automatic)
+        }
+    }
+    
+}
+
+#Preview {
+    TestView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by QinY on 1/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
