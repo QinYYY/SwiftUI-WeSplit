8eb2332e17a554e8ceaeeb6260e6396a371db295
QinYYY <qinyang_c@outlook.com>
2024-07-04 10:11:17 +0930

    add tabview

---
+++ WeSplit/SplitView.swift
@@ -0,0 +1,18 @@
+//
+//  SplitView.swift
+//  WeSplit
+//
+//  Created by QinY on 4/7/2024.
+//
+
+import SwiftUI
+
+struct SplitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplitView()
+}
