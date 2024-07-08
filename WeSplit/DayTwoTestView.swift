ca1bbe119d83ad564dcebb8c2ccb521d32eca33b
QinYYY <qinyang_c@outlook.com>
2024-07-08 09:38:04 +0930

    Guess The Flag

---
+++ WeSplit/DayTwoTestView.swift
@@ -0,0 +1,18 @@
+//
+//  DayTwoTestView.swift
+//  WeSplit
+//
+//  Created by QinY on 4/7/2024.
+//
+
+import SwiftUI
+
+struct DayTwoTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayTwoTestView()
+}
---
+++ WeSplit/GuessFlagView.swift
@@ -0,0 +1,18 @@
+//
+//  GuessFlagView.swift
+//  WeSplit
+//
+//  Created by QinY on 4/7/2024.
+//
+
+import SwiftUI
+
+struct GuessFlagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuessFlagView()
+}
