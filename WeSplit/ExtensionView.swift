58dc0b450afb49c369d6e97b80ea8726135d0d84
QinYYY <qinyang_c@outlook.com>
2024-07-09 11:33:20 +0930

    BetterSleep

---
+++ WeSplit/ExtensionView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtensionView.swift
+//  WeSplit
+//
+//  Created by QinY on 9/7/2024.
+//
+
+import SwiftUI
+
+struct ExtensionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtensionView()
+}
